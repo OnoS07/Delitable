54b82c9a2a2e186872f3417e7a730158882ed206
Ono <soichrooono@gmail.com>
2020-07-09 02:32:12 +0000

    [Add]admin/customer

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,9 +1,24 @@
 class Admins::CustomersController < ApplicationController
-  def index; end
+  def index
+  	@customers = Customer.all
+  end
 
-  def show; end
+  def show
+  	@customer = Customer.find(params[:id])
+  end
 
-  def edit; end
+  def edit
+  	@customer = Customer.find(params[:id])
+  end
 
-  def update; end
+  def update
+   	@customer = Customer.find(params[:id])
+  	@customer.update(customer_params)
+  	redirect_to admins_customer_path(@customer)
+  end
+
+  def customer_params
+    params.require(:customer).permit(:name, :account_name, :profile_image_id, :tel, :postcode,
+                                     :address, :email)
+  end
 end
--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -1,9 +1,18 @@
 class Admins::OrdersController < ApplicationController
   def top
-    @orders = Order.all
+  	@orders = Order.all
   end
 
-  def index; end
+  def index
+  	if params[:customer_id]
+		@customer = Customer.find(params[:customer_id])
+		@orders = Order.where(customer_id: @customer.id)
+		@order_title = "注文一覧/" + @customer.name
+  	else
+  		@orders = Order.all
+  		@order_title = "注文一覧"
+  	end
+  end
 
   def show; end
 
