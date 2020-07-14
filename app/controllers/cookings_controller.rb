a1a33ba6839e375df51f2c18c1ce94c12564712b
Ono <soichrooono@gmail.com>
2020-07-14 06:33:19 +0000

    [Add]ingredient/new

--- app/controllers/cookings_controller.rb
+++ app/controllers/cookings_controller.rb
@@ -1,8 +1,16 @@
 class CookingsController < ApplicationController
   def new
+  	@recipe = Recipe.find(params[:recipe_id])
+  	@ingredients = Ingredient.where(recipe_id: @recipe.id)
+  	@cooking = Cooking.new
+  	@cookings = Cooking.where(recipe_id: @recipe.id)
   end
 
   def create
+  	@cooking = Cooking.new(cooking_params)
+  	@cooking.save
+  	redirect_to new_recipe_cooking_path
+  end
 
   def edit
   end
@@ -11,6 +19,14 @@ class CookingsController < ApplicationController
   end
 
   def destroy
+  	@cooking = Cooking.find(params[:id])
+  	@cooking.destroy
+  	redirect_to new_recipe_cooking_path
+  end
+
+  private
+  def cooking_params
+  	params.require(:cooking).permit(:recipe_id, :cooking_image, :content)
   end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -6,7 +6,7 @@ class Order < ApplicationRecord
   enum payment_method: { 銀行振込: false, クレジットカード: true }
   enum order_status: { 入金待ち: 0, 入金確認: 1, 商品準備中: 2, 発送準備中: 3, 発送済み: 4 }
 
-  validates :postode, presence: true, format: { with: /\A\d{7}\z/ }
+  validates :postcode, presence: true, format: { with: /\A\d{7}\z/ }
   validates :address, presence: true
   validates :name, presence: true
   validates :postage, presence: true, :numericality => { :greater_than_or_equal_to => 0 }
