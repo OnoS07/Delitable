ce38ba200557559a2e1dd64dbe961be190aba866
Ono <soichrooono@gmail.com>
2020-07-27 08:45:10 +0000

    [Fix]migrate

--- db/migrate/20200719100141_add_missing_unique_indices.acts_as_taggable_on_engine.rb
+++ db/migrate/20200719100141_add_missing_unique_indices.acts_as_taggable_on_engine.rb
@@ -8,7 +8,11 @@ AddMissingUniqueIndices.class_eval do
   def self.up
     add_index ActsAsTaggableOn.tags_table, :name, unique: true
 
-    remove_index ActsAsTaggableOn.taggings_table, :tag_id if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
+    # remove_index ActsAsTaggableOn.taggings_table, :tag_id if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id)
+    if index_exists?(ActsAsTaggableOn.taggings_table, :tag_id) #追加
+      remove_foreign_key :taggings, :tags                      #追加
+      remove_index ActsAsTaggableOn.taggings_table, :tag_id    #追加
+    end
     remove_index ActsAsTaggableOn.taggings_table, name: 'taggings_taggable_context_idx'
     add_index ActsAsTaggableOn.taggings_table,
               %i[tag_id taggable_id taggable_type context tagger_id tagger_type],
