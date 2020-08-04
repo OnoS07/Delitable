6006565a31c3358caa5544cfa498b3ba550be2a2
Ono <soichrooono@gmail.com>
2020-08-04 02:02:57 +0000

    [Responsive]recipe/index show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,7 +33,7 @@ $(document).on("turbolinks:load", function(){
       // ナビゲーション矢印の表示(trueで表示)
       arrows : true,
       // スライドショーの自動再生(falseで自動再生なし)
-      autoPlay : false,
+      autoPlay : true,
       // 自動再生時のスライド切替間隔(ミリ秒)
       autoPlayDuration : 4000,
       // キーボードの矢印キーによるスライド送りの設定(trueで有効)
