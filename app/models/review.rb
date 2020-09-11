bfce0414a236085895f9225db03e4daa89b0b82f
Ono <soichrooono@gmail.com>
2020-09-11 09:04:28 +0000

    [Fix]error_message&btn/review_comment

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,6 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :content, presence: true
-  validates :rate, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0 },
-                   presence: true
+  validates :rate, presence: true
+  validates :rate, numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0 }, if: proc{ |s| s.rate.present?}
 end
