b12a34129356db88792542503b4ec43098d5e0f3
Ono <soichrooono@gmail.com>
2020-09-11 14:46:29 +0000

    [Fix]error_message&btn/admin/recipe

--- app/controllers/admins/ingredients_controller.rb
+++ app/controllers/admins/ingredients_controller.rb
@@ -2,13 +2,18 @@ class Admins::IngredientsController < ApplicationController
   before_action :authenticate_admin!
   def edit
     @recipe = Recipe.find(params[:recipe_id])
+
   end
 
   def update
     @ingredient = Ingredient.find(params[:id])
     @recipe = Recipe.find(params[:recipe_id])
-    @ingredient.update(ingredient_params)
-    redirect_back(fallback_location: root_path)
+    if @ingredient.update(ingredient_params)
+      redirect_back(fallback_location: root_path)
+     else
+      redirect_to edit_admins_recipe_ingredient_path(@recipe, @ingredient),:flash=>{error_messages: @ingredient.errors.full_messages}
+    end
+
   end
 
   def destroy
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,7 +2,7 @@ class RecipesController < ApplicationController
   before_action :authenticate_customer!, only: %i[new create edit update destroy]
   before_action :ensure_correct_customer, only: %i[edit update destroy]
   before_action :close_recipe_show, only: [:show]
-  impressionist actions: [:show], unique: %i[impressionable_id ip_address]
+  impressionist actions: [:show]
 
   skip_before_action :verify_authenticity_token
 
@@ -34,7 +34,7 @@ class RecipesController < ApplicationController
       flash[:create] = 'NEW RECIPE CREATE !'
       redirect_to edit_recipe_ingredients_path(@recipe)
     else
-      render 'recipes/new'
+      render :new
     end
   end
 
@@ -94,8 +94,7 @@ class RecipesController < ApplicationController
         flash[:create] = 'UPDATE !'
         redirect_to recipe_path(@recipe)
       else
-        @recipe = Recipe.find(params[:id])
-        render 'recipes/edit'
+        render :edit
       end
     end
   end
